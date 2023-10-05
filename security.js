52017db9fca3449c3b5c00e4da97a12358c2603d
BV0073194 <61207595+BV0073194@users.noreply.github.com>
2023-10-05 22:06:33 +0000

    buuhhhh

---
+++ security.js
@@ -0,0 +1,23 @@
+function generateLockerUI(){
+  var input = document.getElementById("passkeyField");
+  // Execute a function when the user presses a key on the keyboard
+  input.addEventListener("keypress", function(event) {
+    // If the user presses the "Enter" key on the keyboard
+    if (event.key === "Enter") {
+      // Cancel the default action, if needed
+      event.preventDefault();
+      // Trigger the button element with a click
+      document.getElementById("myBtn").click();
+    }
+  });
+}
+
+function UISubmit(){
+  checkServerKeys(passkey = "")
+}
+
+function checkServerKeys(passkey = ""){
+  //perform url request to this at https://gnome.bv007.repl.co/passw-auth
+
+  //recieve valid passwords & cross check with user's input
+}
